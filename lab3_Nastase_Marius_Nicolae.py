f0b680fdc0ee2eaa278d7922104342b7b4b145aa
Nastase Marius <mnastase19@yahoo.com>
2023-11-07 23:24:29 +0200

    modificat ex1&ex2

--- lab3_Nastase_Marius_Nicolae.py
+++ lab3_Nastase_Marius_Nicolae.py
@@ -1,23 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def FFT(x):
-    N = len(x)
-
-    if N == 1:
-        return x
-    else:
-        X_even = FFT(x[::2])
-        X_odd = FFT(x[1::2])
-        factor = \
-            np.exp(-2j * np.pi * np.arange(N) / N)
-
-        X = np.concatenate( \
-            [X_even + factor[:int(N / 2)] * X_odd,
-             X_even + factor[int(N / 2):] * X_odd])
-        return X
-
 def ex1():
     N = 8
 
@@ -36,49 +19,46 @@ def ex1():
 
     print("Este matricea Fourier unitara: ", is_unitary)
 
-    plt.figure(figsize=(12, 6))
+    # Afisare grafice pentru fiecare componenta de frecventa
+    plt.figure(figsize=(12, 8))
 
-    plt.subplot(1, 2, 1)
-    plt.imshow(np.real(F), cmap='hot', interpolation='nearest')
-    plt.title("Partea Reala a Matricei Fourier")
-    plt.colorbar()
-
-    plt.subplot(1, 2, 2)
-    plt.imshow(np.imag(F), cmap='hot', interpolation='nearest')
-    plt.title("Partea Imaginara a Matricei fourier")
-    plt.colorbar()
+    for k in range(N):
+        plt.subplot(N, 2, 2 * k + 1)
+        plt.plot(np.real(F[:, k]))
+        plt.title(f"Partea Reala a Componentei {k + 1}")
+        plt.subplot(N, 2, 2 * k + 2)
+        plt.plot(np.imag(F[:, k]))
+        plt.title(f"Partea Imaginara a Componentei {k + 1}")
+        plt.tight_layout()
 
     plt.show()
 
 def ex2():
-    frecventa_semnal = 2.0 # diferita fata de cea data
-    N = 100 # numarul de esantioane
+    frecventa_semnal = 5  # Frecventa semnalului
+    frecventa_esantionare = 100  # Frecventa de esantionare
 
-    # semnalul sinusoidal
-    timp = np.arange(N)
-    semnal = np.sin(2 * np.pi * frecventa_semnal * timp / N)
+    timp = np.linspace(0, 1, frecventa_esantionare)  # Timpul semnalului
+    x = np.sin(2 * np.pi * frecventa_semnal * timp)
 
-    # Calculati si afisati infasurarea pe cercul unitate pentru Figura1
-    gama = np.linspace(0,1,N)
-    for i in range(N):
-        x = semnal[i] * np.exp(-2 * np.pi * gama[i] * 1j * timp)
-        plt.polar(np.angle(x), np.abs(x), marker = 'o', color = plt.cm.viridis(i / N))
-    plt.title("Figura 1: Infasurarea semnalului pe cercul unitate")
-    plt.show()
+    # Infasurarea semnalului x(t) pe cercul unitate
+    y = x * np.exp(-2j * np.pi * timp)
 
-    gama_values = [0.25,0.5,0.75,frecventa_semnal]
-    plt.figure(figsize=(12,6))
+    fig, axs = plt.subplots(1, 2, figsize=(12, 5))
+    axs[0].plot(timp, x)
+    axs[0].set_title("Semnalul X")
+    axs[0].set_xlabel("Timp")
+    axs[0].set_ylabel("Amplitudine")
+    axs[0].grid()
 
-    for gama_ in gama_values:
-        x = semnal * np.exp(-2 * np.pi * gama_ * 1j * timp)
-        plt.plot(timp, np.abs(x), label=f"Gama = {gama_}")
-    plt.title("Figura 2: Infasurarea semnalului pentru diferite valori ale lui Gama")
-    plt.xlabel("n")
-    plt.ylabel("Amplitudine")
-    plt.legend()
-    plt.grid(True)
+    axs[1].plot(y.real, y.imag, c='green')
+    axs[1].set_title("Reprezentarea semnalului in planul complex (infasurarea)")
+    axs[1].set_xlabel("Real")
+    axs[1].set_ylabel("Imaginar")
+    axs[1].grid()
+
+    plt.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
-    # ex1()
+    #ex1()
     ex2()
