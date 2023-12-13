00c1c585a76556f7b5e8578e72f341822da1a5c9
Nastase Marius <mnastase19@yahoo.com>
2023-12-13 12:06:01 +0200

    lab6 + lab8

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.fft import fft, ifft
+
+
 def ex1():
     N = 100
     x = np.random.rand(N)
@@ -23,5 +26,97 @@ def ex1():
     plt.tight_layout()
     plt.show()
 
+
+def inmultire_polinoame_directa(p, q):
+    # Gradul maxim al rezultatului
+    putere_maxima = len(p) + len(q) - 2
+
+    # Initializare rezultat cu coeficienti 0
+    r = np.zeros(putere_maxima + 1, dtype=int)
+
+    # Inmultirea polinoamelor
+    for i in range(len(p)):
+        for j in range(len(q)):
+            r[i + j] += p[i] * q[j]
+
+    return r
+
+
+def inmultire_polinoame_fft(p, q):
+    # Gradul maxim al rezultatului
+    putere_maxima = len(p) + len(q) - 2
+
+    # Adaugare zerouri la polinoame pentru a se potrivi cu gradul rezultatului
+    p_padded = np.pad(p, (0, putere_maxima - len(p) + 1), mode='constant')
+    q_padded = np.pad(q, (0, putere_maxima - len(q) + 1), mode='constant')
+
+    # Transformata Fourier rapida a celor doua polinoame
+    P = fft(p_padded)
+    Q = fft(q_padded)
+
+    # Inmultirea in domeniul Fourier
+    R = P * Q
+
+    # Transformata Fourier inversa pentru a obtine rezultatul
+    result_fft = np.real(ifft(R))
+
+    # Rotunjire la intreg pentru a evita erori de virgula mobila
+    result_fft = np.round(result_fft).astype(int)
+
+    return result_fft
+def ex2():
+    p = np.random.randint(-10, 10, size=5)
+    q = np.random.randint(-10, 10, size=4)
+
+    # Calculare produs direct
+    result_direct = inmultire_polinoame_directa(p, q)
+    print("Produsul folosind inmultirea directa:", result_direct)
+
+    # Calculare produs folosind FFT
+    result_fft = inmultire_polinoame_fft(p, q)
+    print("Produsul folosind FFT:", result_fft)
+
+def fereastra_dreptunghiulara(Nw):
+    return np.ones(Nw)
+
+def fereastra_Hanning(Nw):
+    return 0.5 * (1 - np.cos(2 * np.pi * np.arange(Nw) / (Nw - 1)))
+
+def plot_fereastra_si_semnal(fereastra, Nw, semnal, titlu):
+    plt.figure(figsize=(10, 4))
+
+    # Afisare fereastra
+    plt.subplot(121)
+    plt.plot(fereastra)
+    plt.title(f'Fereastra {titlu}')
+
+    # Afisare sinusoida trecuta prin fereastra
+    plt.subplot(122)
+    plt.plot(semnal)
+    plt.title(f'Sinusoida trecuta prin fereastra {titlu}')
+
+    plt.tight_layout()
+    plt.show()
+def ex3():
+    Nw = 200
+    f = 100
+    A = 1
+    phi = 0
+    t = np.arange(Nw) / A
+
+    # Construirea sinusoidei
+    signal = A * np.sin(2 * np.pi * f * t + phi)
+
+    # Construirea ferestrelor
+    rectangular = fereastra_dreptunghiulara(Nw)
+    hanning = fereastra_Hanning(Nw)
+
+    # Afisarea graficelor
+    plot_fereastra_si_semnal(rectangular, Nw, signal, 'Dreptunghiulară')
+    plot_fereastra_si_semnal(hanning, Nw, signal, 'Hanning')
+
+
 if __name__ == "__main__":
-    ex1()
+    # ex1()
+    # ex2()
+    ex3()
--- lab8/lab8.py
+++ lab8/lab8.py
@@ -1,1 +1,83 @@
-// lab8 - prezenta Nastase Marius Nicolae 461
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.tsa.arima.model import ARIMA
+
+# Dimensiunea seriei de timp
+N = 1000
+
+# Generăm componente aleatorii pentru trend, sezon și variabilitate mică
+t = np.arange(N)
+trend = 0.02 * t**2  # Componenta trend, ecuație de grad 2
+seasonal = 10 * np.sin(0.02 * np.pi * t) + 5 * np.sin(0.04 * np.pi * t)  # Două componente sezoniere
+noise = np.random.normal(0, 5, N)  # Variatiile mici, zgomot alb gaussian
+
+# Sumăm cele trei componente pentru a obține seria de timp
+time_series = trend + seasonal + noise
+def ex1():
+
+    # Desenăm seriile de timp și componente separat
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(4, 1, 1)
+    plt.plot(t, time_series, label='Seria de timp')
+    plt.legend()
+
+    plt.subplot(4, 1, 2)
+    plt.plot(t, trend, label='Trend')
+    plt.legend()
+
+    plt.subplot(4, 1, 3)
+    plt.plot(t, seasonal, label='Sezon')
+    plt.legend()
+
+    plt.subplot(4, 1, 4)
+    plt.plot(t, noise, label='Variatiile mici')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+def ex2():
+    # Calculăm autocorelația
+    autocorrelation = np.correlate(time_series, time_series, mode='full')
+
+    # Normalizează autocorelația pentru a obține coeficienții de autocorelație
+    autocorrelation /= np.max(autocorrelation)
+
+    # Desenează vectorul de autocorelație
+    plt.figure(figsize=(12, 4))
+    plt.plot(autocorrelation)
+    plt.title('Vectorul de Autocorelație')
+    plt.xlabel('Întârzieri')
+    plt.ylabel('Autocorelație normalizată')
+    plt.show()
+
+    '''
+    Autocorelația măsoară gradul de corelație între o serie de timp și versiunea sa întârziată. Atunci când calculăm 
+    autocorelația pentru o serie de timp la diferite întârzieri, obținem un vector de autocorelație. Acest vector arată 
+    cât de corelate sunt valorile seriei de timp la diferite momente în timp, precum și gradul de repetare al modelelor.
+    '''
+
+def ex3():
+    # Calculăm modelul AR cu o anumită ordine p
+    p = 10
+    model = ARIMA(time_series, order=(p, 0, 0))
+    result = model.fit()
+
+    # Obținem predictiile modelului AR
+    predictions = result.predict(start=p, end=N - 1)
+
+    # Afișăm seria de timp originală și predictiile
+    plt.figure(figsize=(12, 6))
+    plt.plot(t, time_series, label='Seria de timp originală')
+    plt.plot(t[p:], predictions, label=f'Predictii AR(p={p})', color='red', linestyle='dashed')
+    plt.legend()
+    plt.title('Seria de timp originală și predictiile AR')
+    plt.xlabel('Timp')
+    plt.ylabel('Valoare')
+    plt.show()
+
+if __name__ == "__main__":
+    # ex1()
+    # ex2()
+    ex3()
